df113aea4b1bfd24952964455962382bcbad7425
Дмитрий Гордеев <dmitriy.g@ri.domain>
2026-02-21 19:15:17 +0300

    реализация WP-0.1 и WP-0.2

---
+++ src/backend/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/health", () => Results.Ok()).WithName("HealthCheck");
+
+app.Run();
